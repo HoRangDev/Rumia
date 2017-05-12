83f9fb6eca740e2fb51195a3cdd5b2f6563211b0
Yang Kyowon <kyowonhorang@gmail.com>
2017-05-12 13:43:02 +0900

    Added Iteraotr Interface

--- Sources/Rumia/Iterator.h
+++ Sources/Rumia/Iterator.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <vector>
+
+namespace Rumia
+{
+    /* Container Iterator Interface **/
+    template <typename T>
+    class Iterator
+    {
+    public:
+        virtual Iterator& operator++( ) = 0;
+        virtual Iteraotr& operator--( ) = 0;
+        virtual T& operator*( ) = 0;
+        virtual bool operator==( ) = 0;
+        virtual bool operator==( ) = 0;
+
+    };
+}
