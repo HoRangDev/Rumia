ae5e7fe0a0f7268ab270dc5c7229a978c758241c
Yang Kyowon <kyowonhorang@gmail.com>
2017-05-18 20:39:36 +0900

    Added Pair and test cases

---
+++ Sources/Test/PairTest.cpp
@@ -0,0 +1,34 @@
+#include "PairTest.h"
+
+PairTest::PairTest( )
+{
+}
+
+PairTest::~PairTest( )
+{
+}
+
+TEST_F( PairTest, Pair )
+{
+    auto pair = Rumia::Pair<std::string, int>( std::string( "String" ), 3 );
+    EXPECT_EQ( "String", pair.first );
+    EXPECT_EQ( 3, pair.second );
+    EXPECT_EQ( "String", Rumia::Get<0>( pair ) );
+    EXPECT_EQ( 3, Rumia::Get<1>( pair ) );
+}
+
+TEST_F( PairTest, MakePair )
+{
+    auto pair = Rumia::MakePair( std::string( "String" ), 3 );
+    EXPECT_EQ( "String", pair.first );
+    EXPECT_EQ( 3, pair.second );
+    EXPECT_EQ( "String", ( Rumia::Get<0>( pair ) ) );
+    EXPECT_EQ( 3, (Rumia::Get<1>( pair )) );
+    auto get = Rumia::Get<0>( pair );
+
+}
+
+TEST_F( PairTest, TupleSize )
+{
+    EXPECT_EQ( 2, ( Rumia::TupleSize<Rumia::Pair<int, int>>::value ) );
+}
---
+++ Sources/Test/PairTest.h
@@ -0,0 +1,14 @@
+#pragma once
+#if __TEST__
+#include "gtest\gtest.h"
+#include "Rumia\Pair.h"
+
+class PairTest : public testing::Test
+{
+public:
+    PairTest( );
+    ~PairTest( );
+
+};
+
+#endif
